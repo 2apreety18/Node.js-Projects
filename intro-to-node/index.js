f6298535e18f45214fbde9d44cf988cd675015bf
Afia Anjum Preety <2apreety@gmail.com>
2020-09-08 16:24:31 +0600

    my intro to node.js

---
+++ intro-to-node/index.js
@@ -0,0 +1,13 @@
+var superheroes = require("superheroes");
+const supervillains = require('supervillains');
+
+var mySuperheoName = superheroes.random();
+var mySupervillainName = supervillains.random();
+
+console.log(mySuperheoName);
+console.log(mySupervillainName);
+
+
+
+
+
